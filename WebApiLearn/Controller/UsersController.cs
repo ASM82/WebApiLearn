d67e1bb2ac8b2f2b1aa0f82f32d4c49e1bd72107
AlSawi <ahmad.alsawi82@gmail.com>
2022-12-01 12:50:35 +0300

    Add project files.

---
+++ WebApiLearn/Controller/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiLearn.Modules;
+
+namespace WebApiLearn.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            User u1 = new User();
+           // u1.TotalSalary = 90;
+            return new List<User> { 
+            new User{Name="Ahmad", Age=39, Id=1},
+            new User{Name="Omar", Age=39, Id=2},
+
+            };
+        }
+
+
+    }
+}
---
+++ WebApiLearn/Modules/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiLearn.Modules
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        private decimal Salary{ get; set; }
+        public int Age { get; set; }
+        public decimal TotalSalary { get { return Salary - 100; } }
+    }
+}
